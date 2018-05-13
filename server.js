8c70bed14c11c6c57f5c5ec77128e08aeb9d7aa2
Manish M <sheetymanish@gmail.com>
2018-05-13 15:59:08 +0000

    day one

--- server.js
+++ server.js
@@ -266,7 +266,7 @@ app.get("/task1/download", function(req, res) {
                 var extension = filename.split(".")[1];
                 storage.bucket("web-dev-summer-task.appspot.com").file(filename).download({destination: "./assets/files/"+username+"_task1."+extension}).then(() =>{
                     
-                    res.redirect("/files/"+username+"_task1."+extension);
+                    res.redirect("assets/files/"+username+"_task1."+extension);
                     
                     setTimeout(function(){
                         fs.unlink("./assets/files/"+username+"_task1."+extension, function(err) {
